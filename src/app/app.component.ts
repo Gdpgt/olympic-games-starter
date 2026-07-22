32d6087f319eea1aab335485c47eb72f6251c156
Guillaume de Puget <142890016+Gdpgt@users.noreply.github.com>
2026-07-22 17:45:10 +0200

    refactor: centralize data access in OlympicService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { OlympicService } from './core/services/olympic.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private readonly destroyRef = inject(DestroyRef);
 
+  constructor(private olympicService: OlympicService) {}
+
+  ngOnInit(): void {
+    this.olympicService
+      .loadInitialData()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe();
+  }
 }
