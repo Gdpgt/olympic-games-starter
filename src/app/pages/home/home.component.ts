454011d690746661a0cc7472935e425e8f56a79c
Guillaume de Puget <142890016+Gdpgt@users.noreply.github.com>
2026-07-22 17:17:44 +0200

    refactor: add Olympic and Participation interfaces and remove any

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import Chart from 'chart.js/auto';
+import { Olympic } from '../../core/models/olympic';
 
 @Component({
   selector: 'app-home',
@@ -19,15 +20,15 @@ export class HomeComponent implements OnInit {
   constructor(private router: Router, private http:HttpClient) { }
 
   ngOnInit() {
-    this.http.get<any[]>(this.olympicUrl).pipe().subscribe(
+    this.http.get<Olympic[]>(this.olympicUrl).pipe().subscribe(
       (data) => {
         console.log(`Liste des données : ${JSON.stringify(data)}`);
         if (data && data.length > 0) {
-          this.totalJOs = Array.from(new Set(data.map((i: any) => i.participations.map((f: any) => f.year)).flat())).length;
-          const countries: string[] = data.map((i: any) => i.country);
+          this.totalJOs = Array.from(new Set(data.map((olympic) => olympic.participations.map((participation) => participation.year)).flat())).length;
+          const countries: string[] = data.map((olympic) => olympic.country);
           this.totalCountries = countries.length;
-          const medals = data.map((i: any) => i.participations.map((i: any) => (i.medalsCount)));
-          const sumOfAllMedalsYears = medals.map((i) => i.reduce((acc: any, i: any) => acc + i, 0));
+          const medals = data.map((olympic) => olympic.participations.map((participation) => participation.medalsCount));
+          const sumOfAllMedalsYears = medals.map((countryMedals) => countryMedals.reduce((acc, count) => acc + count, 0));
           this.buildPieChart(countries, sumOfAllMedalsYears);
         }
       },
